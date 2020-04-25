3d610038cd52524e079b43afe7de12a5bb5e6248
Bafemo <63488161+Bafemo@users.noreply.github.com>
2020-04-25 18:35:44 -0400

    Add files via upload

---
+++ Instance.tf
@@ -0,0 +1,12 @@
+
+resource "aws_instance" "web" {
+
+ami           = var.ec2_ami
+instance_type = var.ec2_instance_type
+subnet_id     = var.subnet_id_public
+
+tags = {
+
+   Name = "Femi"
+   }
+ }
